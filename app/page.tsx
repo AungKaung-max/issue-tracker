ed9a8df272c1c0206bc36d79d2ec7308e74e6cfc
aungkaungmyathtun <aungkaungmyathtun@gmail.com>
2024-11-25 02:27:26 +0630

    remove page

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,6 @@ export default function Home({
   searchParams: { page: string };
 }) {
   return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    ></Pagination>
+   <div></div>
   );
 }
