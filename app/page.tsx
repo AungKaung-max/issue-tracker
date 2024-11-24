d9dbda8a9a34a9e4e159861f5632a93432836667
aungkaungmyathtun <aungkaungmyathtun@gmail.com>
2024-11-25 03:36:05 +0630

    bar chart complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import LatestIssues from './LatestIssues'
+import React from "react";
+import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
+import IssueChart from "./IssueChart";
 
-const Home = () => {
+const Home = async () => {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
   return (
-    <LatestIssues></LatestIssues>
-  )
-}
+    <>
+      {/* <LatestIssues></LatestIssues>
+      <IssueSummary
+        open={open}
+        closed={closed}
+        inProgress={inProgress}
+      ></IssueSummary> */}
+      <IssueChart open={open}
+        closed={closed}
+        inProgress={inProgress}></IssueChart>
+    </>
+  );
+};
 
-export default Home
+export default Home;
